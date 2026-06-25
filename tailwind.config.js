73014b5d4d586e91c1aae2e117ba365043299afd
mhmwshmhmsh7-ui <mhmwshmhmsh7@gmail.com>
2026-06-26 00:43:11 +0300

    tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#0F4C81",
+        secondary: "#D4AF37",
+        dark: "#0F172A",
+        light: "#F8FAFC"
+      }
+    },
+  },
+  plugins: [],
+}
